dd792e5144ac138946c76bfef67c7a200d9163b7
Piotr  Kubaty <plgkubaty@login01.helios.cyfronet.pl>
2026-07-30 17:49:25 +0200

    clean repo

--- contrastive_finetuning/process.py
+++ contrastive_finetuning/process.py
@@ -1,30 +1,7 @@
-from dataclasses import dataclass
-from typing import Dict, List, Tuple
+from typing import List, Tuple
 
-import numpy as np
 import torch
 
-from rdd.RDD.RDD import RDD
-from rdd_patch.lightglue_masked import LightGlueMasked
-
-@dataclass
-class FrameFeat:
-    keypoints: np.ndarray  # [K, 2]
-    descriptors: np.ndarray  # [K, 256]
-    scores: np.ndarray  # [K]
-    image_size: np.ndarray  # [2] (H, W)
-
-def extract_frame(model: RDD, img: torch.Tensor, device: torch.device, top_k: int) -> FrameFeat:
-    model.top_k = top_k
-    model.set_softdetect(top_k=top_k)
-    img = img.to(device)
-    out = model.extract(img)[0]
-    return FrameFeat(
-        keypoints=out["keypoints"].cpu().numpy(),
-        descriptors=out["descriptors"].cpu().numpy(),
-        scores=out["scores"].cpu().numpy(),
-        image_size=np.array(img.shape[-2:], dtype=np.int32),  # (H, W)
-    )
 
 def pad_to_length(x: torch.Tensor, length: int) -> Tuple[torch.Tensor]:
     if length <= x.shape[-2]:
@@ -52,38 +29,3 @@ def align_tensors_to_max_length(ts: List[torch.Tensor]):
 
     return pts, ms
 
-def get_batched_data(feats: List[FrameFeat], device) -> Dict[str, torch.Tensor]:
-    ks, ds, sizes = [], [], []
-
-    for feat in feats:
-        k = torch.from_numpy(feat.keypoints).to(device)
-        d = torch.from_numpy(feat.descriptors).to(device)
-        size = torch.tensor(feat.image_size[::-1].copy(), device=device)
-
-        ks.append(k)
-        ds.append(d)
-        sizes.append(size)
-
-    ks, masks = align_tensors_to_max_length(ks)
-    ds, _ = align_tensors_to_max_length(ds)
-    sizes = torch.stack(sizes)
-
-    return {
-        "keypoints": ks,
-        "descriptors": ds,
-        "image_size": sizes,
-        "masks": masks.unsqueeze(1),
-    }
-
-def get_matches_lightglue(
-    lg: LightGlueMasked,
-    q_data: Dict[str, torch.Tensor],
-    g_data: Dict[str, torch.Tensor],
-) -> Tuple[float, int]:
-    pred = lg(
-        {
-            "image0": q_data,
-            "image1": g_data,
-        }
-    )
-    return pred
